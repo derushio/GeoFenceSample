00fed14c4831f23735bc121563319346d34d8be7
Derushio <narumi18wa@gmail.com>
2015-06-17 11:35:23 +0900

    大体完成

--- GeoFenceSample/AppDelegate.swift
+++ GeoFenceSample/AppDelegate.swift
@@ -16,6 +16,11 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
+        if (application.respondsToSelector("registerUserNotificationSettings:")) {
+            let settings = UIUserNotificationSettings(forTypes: UIUserNotificationType.Alert | UIUserNotificationType.Sound, categories: nil)
+            application.registerUserNotificationSettings(settings)
+        }
+        
         return true
     }
 
--- GeoFenceSample/ViewController.swift
+++ GeoFenceSample/ViewController.swift
@@ -4,13 +4,14 @@
 //
 //  Created by 中塩成海 on 2015/06/16.
 //  Copyright (c) 2015年 Derushio. All rights reserved.
-//  Supporting Files/Info.plistに注意
+//  Supporting Files/Info.plist, AppDelegate.swiftに注意
 //
 
 import UIKit
 import CoreLocation
 
 class ViewController: UIViewController, CLLocationManagerDelegate {
+// CLLocationManagerDelegateを実装（これによりlocationManagerのdelegateをselfに設定できる）
     
     @IBOutlet weak var geoFenceStatusLabel: UILabel!
     @IBOutlet weak var gpsStatusLabel: UILabel!
@@ -18,18 +19,30 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var latEdit: UITextField!
     @IBOutlet weak var lngEdit: UITextField!
     @IBOutlet weak var radiusEdit: UITextField!
+    // IBOutlet storyboardから関連付けられているオブジェクト
+    // weakは関連付けられたオブジェクトが削除されたら自分も自動的に削除する
     
     var locationManager: CLLocationManager?
+    // ロケーション情報を管理するオブジェクト
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         
         locationManager = CLLocationManager()
+        // locationManagerをCLLocationManagerという型を元にインスタンス（実体化/生成）
+        
         locationManager?.delegate = self
+        // locationManagerのdelegateを自分に設定
+        
         locationManager?.requestAlwaysAuthorization()
+        // 画面外での検知を許可
+        
         locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        // ロケーション更新頻度を最適化してもらう
+        
         locationManager?.startUpdatingLocation()
+        // 自分のロケーション変化を検知スタート（これをコメントアウトするとバッテリー消費量が減る)
     }
 
     override func didReceiveMemoryWarning() {
@@ -38,37 +51,53 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
     }
     
     func locationManager(manager: CLLocationManager!, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+        // 機能の許可情報が変わっていた場合は
         switch (status) {
         case CLAuthorizationStatus.Denied:
-            manager.requestWhenInUseAuthorization()
-            break
+            // 許可されていない場合は
             
+            manager.requestWhenInUseAuthorization()
+            // 許可をリクエストする
+            break
         default:
             break
         }
     }
     
     func locationManager(manager: CLLocationManager!, didStartMonitoringForRegion region: CLRegion!) {
+        // ロケーションモニタリングがスタートしたら
         geoFenceStatusLabel.text = "GeoFenceStart"
     }
     
     func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
+        // ロケーション情報が更新されたら
+        
         var location = locations.last as! CLLocation
         var lat = String(format: "%2.6f", location.coordinate.latitude)
         var lng = String(format: "%2.6f", location.coordinate.longitude)
         gpsStatusLabel.text = "LocationChange " + lat + ", " + lng
+        // ロケーションを表示する
     }
     
     func locationManager(manager: CLLocationManager!, didEnterRegion region: CLRegion!) {
+        // GeoFence内に入ったら
+        
         geoFenceStatusLabel.text = "GeoFence:In"
+        // 表示する
     }
     
     func locationManager(manager: CLLocationManager!, didExitRegion region: CLRegion!) {
+        // GeoFence内から出たら
+        
         geoFenceStatusLabel.text = "GeoFence:Out"
+        // 表示する
     }
     
     func locationManager(manager: CLLocationManager!, monitoringDidFailForRegion region: CLRegion!, withError error: NSError!) {
+        // 失敗したら
+        
         gpsStatusLabel.text = "Error"
+        // エラーを表示する
     }
     
     @IBAction func onStartGeoFenceClick(sender: AnyObject) {
@@ -77,22 +106,41 @@ class ViewController: UIViewController, CLLocationManagerDelegate {
                 locationManager!.stopMonitoringForRegion(region as! CLCircularRegion)
             }
         }
+        UIApplication.sharedApplication().cancelAllLocalNotifications()
+        // 既に登録されている情報を削除
         
         
         var lat = NSString(string: latEdit.text).doubleValue
         var lng = NSString(string: lngEdit.text).doubleValue
         var radius = NSString(string: radiusEdit.text).doubleValue
+        // EditTextFieldから情報を取得
         
         if (0 < lat && 0 < lng && 0 < radius) {
+            // 何も入れられてない場合は0が返ってくる
+            
             var target = CLLocationCoordinate2DMake(lat, lng)
             var identifier = "Point"
+            // ターゲット情報を記述
             
-            var region = CLCircularRegion(center: target, radius: radius, identifier: identifier)
+            var region = CLCircularRegion(circularRegionWithCenter: target, radius: radius, identifier: identifier)
+            // ターゲット情報を元にCLCirclarRegionをインスタンス
+            
             locationManager?.startMonitoringForRegion(region)
+            // locationManagerにregionのモニタリングを開始させる
+            
+            var localNotification = UILocalNotification();
+            localNotification.alertBody = "目的地に着きました";
+            localNotification.soundName = UILocalNotificationDefaultSoundName;
+            localNotification.alertAction = "OK";
+            localNotification.regionTriggersOnce = true;
+            localNotification.region = region;
+            
+            UIApplication.sharedApplication().scheduleLocalNotification(localNotification);
+            // 画面外にいるときはノーティフィケーションに通知する
         } else {
             geoFenceStatusLabel.text = "TextFieldError"
+            // テキストフィールドがおかしいことを通知する
         }
     }
-
 }
 
